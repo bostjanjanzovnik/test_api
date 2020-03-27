bf2607017c4fa42a35fcd11512ad5ad16260a781
Bostjan Janzovnik <bostjan.janzovnik@gmail.com>
2020-03-27 22:20:18 +0100

    fix(middleware): fix validation middleware

--- src/middleware/ValidateSchema.ts
+++ src/middleware/ValidateSchema.ts
@@ -2,7 +2,7 @@ import Joi from '@hapi/joi'
 import { HandledError } from '../errors/HandledError';
 import { ErrorType } from '../interfaces/HandledError';
 
-export const validateSchema = function<T, S extends Joi.ObjectSchema>(object: T, validationSchema: S): void {
+export const validateSchema = <T, S extends Joi.ObjectSchema>(object: T, validationSchema: S): void => {
     const { error } = validationSchema.validate(object);
 
     if (error) {
